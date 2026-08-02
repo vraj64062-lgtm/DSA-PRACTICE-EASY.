d853e27a89d03ebdbb7f38355442b042288ddb65
VIVEK KUMAR. <vraj64062@gmail.com>
2026-08-02 23:04:28 +0530

    Time: 5 ms (80.89%), Space: 46 MB (53.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // Even digits: x == reversedHalf
+        // Odd digits: x == reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
